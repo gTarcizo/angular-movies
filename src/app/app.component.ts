5746142459c1a2ad7fa77340b27b74cc2f2b7bd6
Tarcizo <90656673+gTarcizo@users.noreply.github.com>
2025-06-06 17:00:15 -0300

    3 - adicionando arquivos de loading no proprio arquivo e carregando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,24 +16,7 @@ export class AppComponent {
     setTimeout(() => {
       
       this.movies = [
-        {
-          title:'Batman the dark knight',
-          launchDate: new Date().toLocaleDateString(),
-          price: 423.4,
-          poster : 'https://upload.wikimedia.org/wikipedia/pt/d/d1/The_Dark_Knight.jpg'
-        },
-        {
-          title:'Spider Man 1',
-          launchDate: new Date().toLocaleDateString(),
-          price: 1223.4,
-          poster : 'https://upload.wikimedia.org/wikipedia/pt/1/14/Spide-Man_Poster.jpg'
-        },
-        {
-          title:'Venom',
-          launchDate: new Date().toLocaleDateString(),
-          price: 123.4,
-          poster : 'https://m.media-amazon.com/images/I/813EeINX9RL._AC_UF894,1000_QL80_DpWeblab_.jpg'
-        }
+        
       ]
 
     }, 2000);
